e5b67769240a2872553ef95b8e357aa5a0a6c853
Shuki Vaknin <shukivaknin@gmail.com>
2026-04-10 04:26:06 +0300

    fix: add placeholder image for WordPress sites without screenshot

--- app/Filament/Jabali/Pages/WordPress.php
+++ app/Filament/Jabali/Pages/WordPress.php
@@ -114,7 +114,8 @@ class WordPress extends Page implements HasActions, HasForms, HasTable
                     ->label(__('Preview'))
                     ->state(fn (array $record): ?string => $this->getScreenshotUrl($record['id']))
                     ->height(140)
-                    ->width(220),
+                    ->width(220)
+                    ->defaultImageUrl(url('/images/no-preview.svg')),
                 ViewColumn::make('domain')
                     ->label(__('Site'))
                     ->view('filament.jabali.columns.wordpress-site'),
